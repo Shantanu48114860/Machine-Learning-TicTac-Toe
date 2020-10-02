cc6fec05b329f81a97b849d6795aa8bb46bf5372
Shantanu Ghosh <beingshantanu2406@gmail.com>
2020-10-02 19:23:57 -0400

    updated.

--- Regressor.py
+++ Regressor.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import svm
-from sklearn.model_selection import cross_val_score, GridSearchCV, KFold
+from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
 
@@ -32,7 +30,7 @@ class Regressor:
         for k_value in regressor_dict:
             print("k value in multiclass knn -> " + str(k_value))
             knn_regressor = regressor_dict[k_value]
-            y_pred = knn_regressor.predict(np_x_test) # 1311 x 9
+            y_pred = knn_regressor.predict(np_x_test)  # 1311 x 9
             y_pred_list.append(y_pred)
 
             y_pred_fixed = np.where(y_pred > 0.5, 1, 0)
@@ -61,19 +59,16 @@ class Regressor:
         folds = KFold(n_splits=5, shuffle=True, random_state=1)
         param_grid = [
             {
-                "learning_rate_init": [1e-04, 1e-05, 1e-02],
                 'max_iter': [500, 1000],
                 'hidden_layer_sizes': [
-                    (200, 200, 9), (250, 250, 9), (100, 100, 9)
+                    (200, 200, 9)
                 ]
             }
         ]
 
         clf = GridSearchCV(MLPRegressor(random_state=1,
                                         activation='relu',
-                                        solver='adam',
-                                        alpha=1e-5,
-                                        learning_rate_init=1e-04),
+                                        solver='adam'),
                            param_grid, cv=folds)
         clf.fit(np_x_train, np_y_train)
         best_score = clf.best_score_
@@ -82,44 +77,27 @@ class Regressor:
         print(clf.best_params_)
 
         best_hyperparams = clf.best_params_
-        # print(best_hyperparams)
-        # clf = MLPRegressor(solver='adam', alpha=1e-5,
-        #                    hidden_layer_sizes=(200, 200, 9),
-        #                    random_state=1,
-        #                    max_iter=1000)
-        clf.fit(np_x_train, np_y_train)
-        # y_pred = clf.predict(np_x_test)
-        # y_pred_fixed = np.where(y_pred > 0.5, 1, 0)
-        # y_pred_mean = np.where(y_pred > np.mean(y_pred), 1, 0)
-        #
-        # accuracy_fixed = Utils.get_accuracy_score(np_y_test, y_pred_fixed)
-        # print(accuracy_fixed)
-        #
-        # accuracy_mean = Utils.get_accuracy_score(np_y_test, y_pred_mean)
-        # print(accuracy_mean)
+        learning_rate_init = best_hyperparams["learning_rate_init"]
+        max_iter = best_hyperparams["max_iter"]
+        hidden_layer_sizes = best_hyperparams["hidden_layer_sizes"]
 
-        # best_score = clf.best_score_
-        # print("Best parameters set found on development set:")
-        # print(clf.best_params_)
-        #
-        # best_hyperparams = clf.best_params_
-        # print("The best test score is {0} corresponding to hyperparameters {1}"
-        #       .format(best_score, best_hyperparams))
-        # activation = best_hyperparams['activation']
-        # hidden_layer_sizes = best_hyperparams['hidden_layer_sizes']
-        #
-        # final_clf = MLPRegressor(max_iter=100, activation=activation,
-        #                           hidden_layer_sizes=hidden_layer_sizes)
-        # final_clf.fit(np_x_train, np_y_train)
-        #
-        # y_pred = final_clf.predict(np_x_test)
-        #
-        # y_pred_fixed = np.where(y_pred > 0.5, 1, 0)
-        # y_pred_mean = np.where(y_pred > np.mean(y_pred), 1, 0)
-        #
-        # accuracy_fixed = Utils.get_accuracy_score(np_y_test, y_pred_fixed)
-        # print("Accuracy linear MLP with fixed threshold: {0}".format(accuracy_fixed))
-        #
-        # accuracy_mean = Utils.get_accuracy_score(np_y_test, y_pred_mean)
-        # print("Accuracy linear MLP with mean threshold: {0}".format(accuracy_mean))
-        # print("---" * 5)
+        final_clf = MLPRegressor(random_state=1, max_iter=1000, activation='relu',
+                                 hidden_layer_sizes=hidden_layer_sizes,
+                                 solver='adam')
+
+        # final_clf = MLPRegressor(random_state=1, max_iter=1000, activation='relu',
+        #                          # learning_rate_init=learning_rate_init,
+        #                         hidden_layer_sizes=(200, 200, 9))
+
+        final_clf.fit(np_x_train, np_y_train)
+        y_pred = final_clf.predict(np_x_test)
+
+        y_pred_fixed = np.where(y_pred > 0.5, 1, 0)
+        y_pred_mean = np.where(y_pred > np.mean(y_pred), 1, 0)
+
+        accuracy_fixed = Utils.get_accuracy_score(np_y_test, y_pred_fixed)
+        print("Accuracy linear MLP with fixed threshold: {0}".format(accuracy_fixed))
+
+        accuracy_mean = Utils.get_accuracy_score(np_y_test, y_pred_mean)
+        print("Accuracy linear MLP with mean threshold: {0}".format(accuracy_mean))
+        print("---" * 5)
